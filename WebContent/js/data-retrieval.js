0032c513ff57c4784e5d0378ec8e1199a5682ead
drusk <drusk@uvic.ca>
2012-10-23 13:19:42 -0700

    Added jsonp request for yahoo symbol suggestions.

--- WebContent/js/data-retrieval.js
+++ WebContent/js/data-retrieval.js
@@ -6,11 +6,52 @@
 
 $(document).ready(initEventHandlers);
 
+/**
+ * Initializes the application's UI event handlers.
+ */
 function initEventHandlers() {
 	$("#search-submit").click(searchSubmitHandler);
 }
 
+/**
+ * Handles events when the user submits a search.
+ */
 function searchSubmitHandler() {
 	var query = $("#search-input").val();
 	alert(query);
+	
+	if (query.length == 0) {
+		/* No query has been entered, do nothing */
+		return;
+	}
+	
+	var symbols = yahooSuggestSymbols(query);
+	
+}
+
+/**
+ * Retries symbol suggestions for a name queried.
+ * 
+ * @param query
+ */
+function yahooSuggestSymbols(query) {
+	/* The callback doesn't work if YAHOO isn't global. */
+	window.YAHOO = {
+		Finance: {
+			SymbolSuggest: {}
+		}
+	};
+	
+	$.ajax({
+		type: "GET",
+		url: "http://d.yimg.com/autoc.finance.yahoo.com/autoc",
+		data: {query: query},
+		dataType: "jsonp",
+		jsonp: "callback",
+		jsonpCallback: "YAHOO.Finance.SymbolSuggest.ssCallback",
+	});
+
+	YAHOO.Finance.SymbolSuggest.ssCallback = function (data) {
+        alert(JSON.stringify(data));
+    };
 }
